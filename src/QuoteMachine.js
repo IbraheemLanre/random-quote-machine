26626cdc63c48704db0f00d27851091994bdc738
Ibraheem <talk2lanre205@yahoo.com>
2020-05-13 18:55:02 +0200

    updated with more functional

--- src/QuoteMachine.js
+++ src/QuoteMachine.js
@@ -1,73 +1,35 @@
-import React, {Component} from 'react';
-import {random} from 'lodash';
-import App from './App';
-import 'typeface-roboto';
-import {Grid} from '@material-ui/core';
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faTwitter} from '@fortawesome/free-brands-svg-icons';
 
 
-const styles = {
-  container: {
-    alignItems: 'center',
-    display: 'flex',
-    height: '100vh',
-  }
-};
-
-
-class QuoteMachine extends Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      quotes:[],
-      selectedQuoteIndex: null,
-    }
-    this.assignNewQuoteIndex = this.assignNewQuoteIndex.bind(this);
-    this.generateNewQuoteIndex = this.generateNewQuoteIndex.bind(this);
-  }
-
-  componentDidMount() {
-    fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
-      .then(data => data.json())
-      .then(quotes => this.setState({quotes}, this.assignNewQuoteIndex));
-  }
-
-  get selectedQuote(){
-    if(!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)) {
-      return undefined;
-    }
-    return this.state.quotes[this.state.selectedQuoteIndex];
-  }
-
-  /**
-  * Returns as integer representing an index in state.quotes
-  * If state.quotes is empty, returns undefined
-
-  */
-
-    generateNewQuoteIndex() {
-      //install package loadash as (npm install loadash)
-      if(!this.state.quotes.length){
-        return;
-      }
-      return random(0, this.state.quotes.length-1 );
-    }
-
-    assignNewQuoteIndex() {
-      this.setState({ selectedQuoteIndex: this.generateNewQuoteIndex() });
-    }
-
-  render() {
-
-    return (
-    <Grid className={this.props.classes.container} id="quote-box" justify="center" container>
-      <Grid item>
-      <App selectedQuote = {this.selectedQuote} assignNewQuoteIndex={this.assignNewQuoteIndex}/>
-      </Grid>
-
-    </Grid>
+const QuoteMachine = ({selectedQuote, assignNewQuoteIndex}) =>  (
+    <Card>
+      <CardContent>
+      <h1> Get Inspired with beautiful Quotes </h1>
+           <Typography id="text">
+             {selectedQuote.quote} - <span id="author">{selectedQuote.author}</span>
+          </Typography>
+      </CardContent>
+      <CardActions>
+        <Button  id="new-quote" size='small' onClick={assignNewQuoteIndex}>New Quote</Button>
+        <IconButton id="tweet-quote"
+        target="_blank"
+        href={encodeURI(`https://twitter.com/intent/tweet?text=${selectedQuote.quote}&hashtags=ibraheemolaadua`)}
+        >
+          
+          <FontAwesomeIcon icon={faTwitter} size='md'></FontAwesomeIcon>
+        </IconButton>
+      </CardActions>
+      
+    </Card>
   );
-  }
-}
 
-export default withStyles(styles)(QuoteMachine);
+
+export default QuoteMachine;
